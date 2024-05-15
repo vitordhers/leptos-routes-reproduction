a2d57dfbf7a05d64cf93a1dfb6f0df707cfe0d89
Vitor Dhers <vitor.dhers@gmail.com>
2024-05-14 22:20:03 -0300

    adds fragment issue

---
+++ src/lib.rs
@@ -0,0 +1,115 @@
+use leptos::*;
+use leptos_router::*;
+use leptos_meta::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    provide_meta_context();
+
+    view! {
+        <h1>"Router issue reproduction App"</h1>
+        <Router>
+            <Routes>
+                // this works ok
+                
+                // <Route path="/" view=|| view! {<Html lang="en-US" /><Outlet />}>
+                //     <Route path="/" view=|| view! {<SimpleNavComponent />}>
+                //         { view! {<DeepNestedRouteSelector url=1 />} }
+                //         { view! {<DeepNestedRouteSelector url=2 />} }
+                //     </Route>
+                //     <Route path="/comp3" view=SimpleComponent3 />
+                //     <Route path="/comp4" view=SimpleComponent4 />
+                // </Route>
+                
+                // this one also works ok
+                <EnumerationNestedRoutes />
+                
+                // this doesn't work, tough
+                // <FragmentNestedRoutes />
+            </Routes>
+        </Router>
+    }
+}
+
+#[component()]
+fn SimpleNavComponent() -> impl IntoView {
+    view! {
+        <nav>
+            <A href="/comp1">"Component 1"</A>
+            <A href="/comp2">"Component 2"</A>
+            <A href="/comp3">"Component 3"</A>
+            <A href="/comp4">"Component 4"</A>
+        </nav>
+        <Outlet />
+    }
+}
+
+#[component()]
+fn SimpleComponent1() -> impl IntoView {
+    view! {<h1>"This is simple component 1"</h1>}
+}
+
+#[component()]
+fn SimpleComponent2() -> impl IntoView {
+    view! {<h1>"This is simple component 2"</h1>}
+}
+
+#[component()]
+fn SimpleComponent3() -> impl IntoView {
+    view! {<h1>"This is simple component 3"</h1>}
+}
+
+#[component()]
+fn SimpleComponent4() -> impl IntoView {
+    view! {<h1>"This is simple component 4"</h1>}
+}
+
+#[component(transparent)]
+fn DeepNestedRouteSelector(#[prop(default = 1)] url: i8) -> impl IntoView {
+    let path = match url {
+        1 => "/comp1",
+        _ => "/comp2",
+    };
+
+    view! {
+        <Route path=path view=move || match url {
+            1 => view! {<SimpleComponent1 />},
+            _ => view! {<SimpleComponent2 />},
+        } />
+    }
+}
+
+#[component(transparent)]
+fn EnumerationNestedRoutes() -> impl IntoView {
+    view! {
+        <Route path="/" view=|| view! {<Html lang="en-US" /><Outlet />}>
+            <Route path="/" view=|| view! {<SimpleNavComponent />}>
+                { view! {<DeepNestedRouteSelector url=1 />} }
+                { view! {<DeepNestedRouteSelector url=2 />} }
+            </Route>
+            <Route path="/comp3" view=SimpleComponent3 />
+            <Route path="/comp4" view=SimpleComponent4 />
+        </Route>
+    }
+}
+
+#[component(transparent)]
+fn FragmentNestedRoutes() -> impl IntoView {
+    view! {
+        <Route path="/" view=|| view! {<Html lang="en-US" /><Outlet />}>
+            <Route path="/" view=|| view! {<SimpleNavComponent />}>
+                {Fragment::from_iter(vec![1, 2].into_iter().map(|n: i8| view!{ <DeepNestedRouteSelector url=n /> }))}
+            </Route>
+            <Route path="/comp3" view=SimpleComponent3 />
+            <Route path="/comp4" view=SimpleComponent4 />
+        </Route>
+    }
+}
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    console_error_panic_hook::set_once();
+
+    mount_to_body(App);
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+
+use leptos::*;
+
+#[cfg(feature = "ssr")]
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_files::Files;
+    use actix_web::*;
+    use app_routes::App;
+    use leptos_actix::{generate_route_list, LeptosRoutes};
+
+    let conf = get_configuration(None).await.unwrap();
+    let addr = conf.leptos_options.site_addr;
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(App);
+    println!("listening on http://{}", &addr);
+    println!("routes {:?}", routes);
+
+    HttpServer::new(move || {
+        let leptos_options = &conf.leptos_options;
+        let site_root = &leptos_options.site_root;
+
+        App::new()
+            // serve JS/WASM/CSS from `pkg`
+            .service(Files::new("/pkg", format!("{site_root}/pkg")))
+            // serve other assets from the `assets` directory
+            .service(Files::new("/assets", site_root))
+            .leptos_routes(leptos_options.to_owned(), routes.to_owned(), App)
+            .app_data(web::Data::new(leptos_options.to_owned()))
+        //.wrap(middleware::Compress::default())
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
+
+#[cfg(not(any(feature = "ssr", feature = "csr")))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
+    // see optional feature `csr` instead
+}
